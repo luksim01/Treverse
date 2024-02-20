6f5cd3c1d9b8c0e3c65876224b5411347449a011
chriswastyping <76431488+chriswastyping@users.noreply.github.com>
2024-02-20 18:21:01 +0000

    Fog

---
+++ Assets/PowerPlantCollider.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerPlantCollider : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    [ContextMenu("Add MeshColliders")]
+    private void AddMeshColliders()
+    {
+        if (_target == null) _target = transform;
+        AddRecursively(_target);
+    }
+
+    private void AddRecursively(Transform parent)
+    {
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+            if (child.GetComponent<MeshFilter>() != null) child.gameObject.AddComponent<MeshCollider>();
+
+            AddRecursively(child);
+        }
+    }
+}
