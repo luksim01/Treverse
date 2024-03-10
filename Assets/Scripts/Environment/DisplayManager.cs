ba5a3109a1a80ddcbdc611284da2f94cd38bef3f
luksim01 <145286746+luksim01@users.noreply.github.com>
2024-03-10 21:04:04 +0000

    added options for glow types
    - pulsing and flicker options added
    - original material is no longer being changed on playback exit, a copy is created in the script during the run that is updated by it

--- Assets/Scripts/Environment/DisplayManager.cs
+++ Assets/Scripts/Environment/DisplayManager.cs
@@ -5,26 +5,97 @@ using UnityEngine.UI;
 
 public class DisplayManager : MonoBehaviour
 {
-    public Image lowBatteryDisplay;
-    private Material lowBatteryDisplayMaterial;
-    private float glowSaturation;
-    public float changeSpeed = 200f;
+    // solar battery display
+    public Image solarBatteryDisplay;
+    private Material solarBatteryDisplayMaterial;
+
+    // display glow options
+    private enum DisplayGlow
+    {
+        pulsing,
+        flickering
+    }
+
+    [SerializeField] private DisplayGlow displayGlow;
+
+    // pulsing glow
+    private float pulseGlowSpeed = 1200f;
+    private float minimumGlowLevel = 200f;
+    private float maximumGlowLevel = 1400f;
+
+    // flickering glow
+    private bool isFlickering = false;
+    private float flickerGlowSpeed = 30000f;
+    private float stableGlowSpeed = 300f;
+    private float minimumStableGlowLevel = 1100f;
+    private float maximumStableGlowLevel = 1400f;    
 
     void Start()
     {
-        lowBatteryDisplayMaterial = lowBatteryDisplay.material; // luksim not working
-        glowSaturation = lowBatteryDisplayMaterial.GetFloat("_GlowSaturation");
-        Debug.Log("glowSaturation " + glowSaturation);
+        // solar battery display material
+        solarBatteryDisplayMaterial = new Material(solarBatteryDisplay.material);
+
+        // display, flicker mode, stable glow time
+        Invoke("StableGlow", 3);
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        glowSaturation += changeSpeed * Time.deltaTime;
-        lowBatteryDisplayMaterial.SetFloat("_GlowSaturation", glowSaturation);
+        switch (displayGlow)
+        {
+            case DisplayGlow.pulsing:
+                // gradual pulsing glow
+                pulseGlowSpeed = PulsingGlow(solarBatteryDisplayMaterial, minimumGlowLevel, maximumGlowLevel, pulseGlowSpeed);
+                break;
+            case DisplayGlow.flickering:
+                if (isFlickering)
+                {
+                    // fast pulsing glow creates flicker
+                    flickerGlowSpeed = PulsingGlow(solarBatteryDisplayMaterial, minimumGlowLevel, maximumGlowLevel, flickerGlowSpeed);
+                }
+                else
+                {
+                    // slow pulsing glow creates stable glow
+                    stableGlowSpeed = PulsingGlow(solarBatteryDisplayMaterial, minimumStableGlowLevel, maximumStableGlowLevel, stableGlowSpeed);
+                }
+                break;
+        }
+    }
 
-        if(glowSaturation >= 1400 || glowSaturation <= 200)
+    void StableGlow()
+    {
+        isFlickering = true;
+        StartCoroutine(FlickerGlow());
+
+        float stableTime = Random.Range(3, 6);
+        Invoke("StableGlow", stableTime);
+    }
+
+    IEnumerator FlickerGlow()
+    {
+        float flickerTime = Random.Range(0.2f, 0.6f);
+        yield return new WaitForSeconds(flickerTime);
+        isFlickering = false;
+    }
+
+    private float PulsingGlow(Material displayMaterial, float minimumGlowLevel, float maximumGlowLevel, float changeSpeed)
+    {
+        float glowSaturation = displayMaterial.GetFloat("_GlowSaturation") + (changeSpeed * Time.deltaTime);
+        
+        if (glowSaturation > maximumGlowLevel)
         {
             changeSpeed = -changeSpeed;
+            glowSaturation = maximumGlowLevel;
         }
+        else if (glowSaturation < minimumGlowLevel)
+        {
+            changeSpeed = -changeSpeed;
+            glowSaturation = minimumGlowLevel;
+        }
+
+        displayMaterial.SetFloat("_GlowSaturation", glowSaturation);
+        solarBatteryDisplay.material = displayMaterial;
+
+        return changeSpeed;
     }
 }
