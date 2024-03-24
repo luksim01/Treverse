7eb4f9459f7fa55a82f8d8832d74d22db51fabbd
luksim01 <145286746+luksim01@users.noreply.github.com>
2024-03-24 20:13:40 +0000

    pulsing glowing outline within vision
    - glowing outline around interactive objects within range
    - glowing outline pulses

--- Assets/Scripts/Environment/Interactor.cs
+++ Assets/Scripts/Environment/Interactor.cs
@@ -17,8 +17,16 @@ public class Interactor : MonoBehaviour
     Material originalMaterial;
     GameObject previouslyOutlined;
 
-    //public Material blackRubber;
-    //public Material glow;
+    public Material glowingOutline;
+
+    // pulsing glow
+    [SerializeField] private float pulseGlowSpeed = 70f;
+    [SerializeField] private float minimumGlowLevel = 10f;
+    [SerializeField] private float maximumGlowLevel = 80f;
+
+    private void Start()
+    {
+    }
 
     void Outline(GameObject seenObject)
     {
@@ -27,11 +35,10 @@ public class Interactor : MonoBehaviour
             ClearOutline();
             originalMaterial = seenObject.GetComponent<MeshRenderer>().sharedMaterial;
             seenObject.GetComponent<MeshRenderer>().sharedMaterial = outlineMaterial;
-            //seenObject.GetComponent<MeshRenderer>().SetMaterials();
             previouslyOutlined = seenObject;
-    }
+        }
 
-}
+    }
 
     void ClearOutline()
     {
@@ -42,7 +49,40 @@ public class Interactor : MonoBehaviour
         }
     }
 
-    void Update()
+    void AddOutline(GameObject seenObject)
+    {
+        if (previouslyOutlined != seenObject)
+        {
+            RemoveOutline();
+            List<Material> seenObjectMaterials = new List<Material>();
+            seenObject.GetComponent<MeshRenderer>().GetMaterials(seenObjectMaterials);
+
+            originalMaterial = seenObjectMaterials[0];
+
+            seenObjectMaterials.Add(glowingOutline);
+            seenObject.GetComponent<MeshRenderer>().SetMaterials(seenObjectMaterials);
+            previouslyOutlined = seenObject;
+        }
+    }
+
+    void PrintMaterials(List<Material> materialsList)
+    {
+        for (int i = 0; i < materialsList.Count; i++)
+        {
+            Debug.Log("materialsList[" + i + "]: " + materialsList[i]);
+        }
+    }
+
+    void RemoveOutline()
+    {
+        if (previouslyOutlined != null)
+        {
+            previouslyOutlined.GetComponent<MeshRenderer>().SetMaterials(new List<Material>() { originalMaterial });
+            previouslyOutlined = null;
+        }
+    }
+
+    void FixedUpdate()
     {
         Ray lineOfSight = new Ray(lineOfSightSource.position, lineOfSightSource.forward);
         bool hasSeenObject = Physics.SphereCast(lineOfSight, sightRadius, out RaycastHit hitInfo, sightRange);
@@ -58,7 +98,15 @@ public class Interactor : MonoBehaviour
 
         if (hasSeenObject && isInteractiveObject)
         {
-            Outline(hitInfo.collider.gameObject);
+            GameObject seenObject = hitInfo.collider.gameObject;
+            AddOutline(seenObject);
+
+
+            // glowwing outline pulse
+            List<Material> seenObjectMaterials = new List<Material>();
+            seenObject.GetComponent<MeshRenderer>().GetMaterials(seenObjectMaterials);
+            Material glowingOutlineMaterial = seenObjectMaterials[seenObjectMaterials.Count-1];
+            pulseGlowSpeed = PulsingGlow(glowingOutlineMaterial, minimumGlowLevel, maximumGlowLevel, pulseGlowSpeed);
 
             if (Input.GetKeyDown(KeyCode.I))
             {
@@ -67,7 +115,27 @@ public class Interactor : MonoBehaviour
         }
         else
         {
-            ClearOutline();
+            RemoveOutline();
+        }        
+    }
+
+    private float PulsingGlow(Material displayMaterial, float minimumGlowLevel, float maximumGlowLevel, float changeSpeed)
+    {
+        float glowSaturation = displayMaterial.GetFloat("_GlowSaturation") + (changeSpeed * Time.deltaTime);
+
+        if (glowSaturation > maximumGlowLevel)
+        {
+            changeSpeed = -changeSpeed;
+            glowSaturation = maximumGlowLevel;
         }
+        else if (glowSaturation < minimumGlowLevel)
+        {
+            changeSpeed = -changeSpeed;
+            glowSaturation = minimumGlowLevel;
+        }
+
+        displayMaterial.SetFloat("_GlowSaturation", glowSaturation);
+
+        return changeSpeed;
     }
 }
