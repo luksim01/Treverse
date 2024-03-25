0c07f4991c800175baa0982511e5de4c7c455fbc
luksim01 <145286746+luksim01@users.noreply.github.com>
2024-03-25 22:02:13 +0000

    connector pulled towards player
    - when press interact key I, the connector flies towards the player.
    - buoyancy added to connector when interacted with in case it flies past player to give them a chance to collect it.

--- Assets/Scripts/Environment/Interactive.cs
+++ Assets/Scripts/Environment/Interactive.cs
@@ -4,8 +4,45 @@ using UnityEngine;
 
 public class Interactive : MonoBehaviour, IInteractive
 {
+    Rigidbody gameObjectsRigidbody;
+    [SerializeField] private float upwardForceMultiplier = 1f;
+    [SerializeField] private float rotationalForceMultiplier = 1f;
+    [SerializeField] private float upwardOffset = 1f;
+    GameObject kayak;
+
+    private void Start()
+    {
+        kayak = GameObject.Find("Kayak");
+    }
+
     public void Interact()
     {
-        Debug.Log(Random.Range(0, 100));
+        if (gameObject.GetComponent<Rigidbody>() != null)
+        {
+            gameObjectsRigidbody = gameObject.GetComponent<Rigidbody>();
+        }
+        else
+        {
+            gameObjectsRigidbody = gameObject.AddComponent<Rigidbody>();
+        }
+
+        gameObjectsRigidbody.isKinematic = false;
+
+        Vector3 direction = kayak.transform.position - gameObject.transform.position;
+        gameObjectsRigidbody.AddForce(new Vector3(direction.x, direction.y + upwardOffset, direction.z) * upwardForceMultiplier, ForceMode.Impulse);
+        gameObjectsRigidbody.AddRelativeTorque(new Vector3(0.25f, 0.5f, 1f) * rotationalForceMultiplier);
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == kayak)
+        {
+            Destroy(gameObject);
+        }
     }
 }
